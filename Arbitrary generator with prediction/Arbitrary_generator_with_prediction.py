96350b6534f6d656cd6e239db7c32e19eaec90e5
DANIEL NTIRANDEKURA <frienzik@gmail.com>
2023-04-20 22:26:27 +0300

    Change commets

--- Arbitrary generator with prediction/Arbitrary_generator_with_prediction.py
+++ Arbitrary generator with prediction/Arbitrary_generator_with_prediction.py
@@ -2,24 +2,24 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
-# Генерируем произвольные данные
+# Generating arbitrary data
 x = np.arange(0, 10, 0.1)
 y = np.sin(x)
 
-# Создаем модель нейронной сети
+# Creating a neural network model
 model = tf.keras.Sequential([
   tf.keras.layers.Dense(10, activation='relu', input_shape=(1,)),
   tf.keras.layers.Dense(1)
 ])
 
-# Компилируем модель и обучаем на данных
+# Compile the model and train on the data
 model.compile(optimizer='adam', loss='mse')
 model.fit(x, y, epochs=350)
 
-# Предсказываем значения на тех же данных
+# Predicting values on the same data
 y_pred = model.predict(x)
 
-# Строим график
+# Plotting a graph
 plt.plot(x, y, label='True function')
 plt.plot(x, y_pred, label='Predicted function')
 plt.legend()
